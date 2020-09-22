41721ac05718e90c87563130a07ccad5c0322e49
Fahad Hayat <fahadhayat@outlook.com>
2020-09-22 19:55:35 -0400

    Updates

---
+++ precache-manifest.4d3558a8f03ad006668d1b9b0f9c65a9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7de27fb64f425079d64c147351789b",
+    "url": "fahadhayat.life/index.html"
+  },
+  {
+    "revision": "920f9084702445b70116",
+    "url": "fahadhayat.life/static/css/main.dc17a8a3.chunk.css"
+  },
+  {
+    "revision": "130feb00d1128b83ec29",
+    "url": "fahadhayat.life/static/js/2.85b68806.chunk.js"
+  },
+  {
+    "revision": "baebdd1671010f55d6a3b55cb359d81e",
+    "url": "fahadhayat.life/static/js/2.85b68806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920f9084702445b70116",
+    "url": "fahadhayat.life/static/js/main.840ff9b1.chunk.js"
+  },
+  {
+    "revision": "9dd3ff162f271f44adb8",
+    "url": "fahadhayat.life/static/js/runtime-main.9dfd395a.js"
+  },
+  {
+    "revision": "a9be81f6e37865942ac800f980aa2a5e",
+    "url": "fahadhayat.life/static/media/fahad_hayat.a9be81f6.jpg"
+  },
+  {
+    "revision": "cef50fc165dd17d6062e0b8c2b05fbf6",
+    "url": "fahadhayat.life/static/media/fahadhayat.cef50fc1.pdf"
+  },
+  {
+    "revision": "a5e87149af70db8b7102e53a6fdb02b6",
+    "url": "fahadhayat.life/static/media/up-arrow-circle.a5e87149.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.00abae252766122eb602a8b0c1b4c36f.js"
+  "fahadhayat.life/precache-manifest.4d3558a8f03ad006668d1b9b0f9c65a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("fahadhayat.life/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
