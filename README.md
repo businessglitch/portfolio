3c34fa3dc75765e194bc0752385fe54ed3c6921f
Fahad Hayat <fahadhayat@outlook.com>
2020-09-22 18:47:41 -0400

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# portfolio
-Finalized portfolio version 
-
-Design Inspired by: <br/>
-Akram - https://www.youtube.com/channel/UCqrxiLP9RHz2GxDJaZuTRBw <br/>
-Masooma Ali - https://www.masoomaali.com/ <br/>
-Omar Flores - https://omarflores.dev/ <br/>
---
+++ precache-manifest.00abae252766122eb602a8b0c1b4c36f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d87a34f8830996c7d1af26754b42a9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e850ba197340b8ddcf40",
+    "url": "/portfolio/static/css/main.dc17a8a3.chunk.css"
+  },
+  {
+    "revision": "130feb00d1128b83ec29",
+    "url": "/portfolio/static/js/2.85b68806.chunk.js"
+  },
+  {
+    "revision": "baebdd1671010f55d6a3b55cb359d81e",
+    "url": "/portfolio/static/js/2.85b68806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e850ba197340b8ddcf40",
+    "url": "/portfolio/static/js/main.289fba21.chunk.js"
+  },
+  {
+    "revision": "91ab8cc48ed2d3fba70a",
+    "url": "/portfolio/static/js/runtime-main.29fb0dd0.js"
+  },
+  {
+    "revision": "a9be81f6e37865942ac800f980aa2a5e",
+    "url": "/portfolio/static/media/fahad_hayat.a9be81f6.jpg"
+  },
+  {
+    "revision": "cef50fc165dd17d6062e0b8c2b05fbf6",
+    "url": "/portfolio/static/media/fahadhayat.cef50fc1.pdf"
+  },
+  {
+    "revision": "a5e87149af70db8b7102e53a6fdb02b6",
+    "url": "/portfolio/static/media/up-arrow-circle.a5e87149.svg"
+  }
+]);
