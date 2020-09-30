df329e8fa6bd892ea802a0d0221191f04b3c7aef
Fahad Hayat <fahadhayat@outlook.com>
2020-09-30 09:55:46 -0400

    Updates

---
+++ precache-manifest.6407aaa7decc71ad9567af80322d48a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4092763fcdd3ba0bc925cf1eb9526a81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f809855019fdf50f53e4",
+    "url": "/static/css/main.02ebda2f.chunk.css"
+  },
+  {
+    "revision": "130feb00d1128b83ec29",
+    "url": "/static/js/2.85b68806.chunk.js"
+  },
+  {
+    "revision": "baebdd1671010f55d6a3b55cb359d81e",
+    "url": "/static/js/2.85b68806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f809855019fdf50f53e4",
+    "url": "/static/js/main.ce9b5368.chunk.js"
+  },
+  {
+    "revision": "27e277ca3f130b8f1fce",
+    "url": "/static/js/runtime-main.ad540005.js"
+  },
+  {
+    "revision": "a9be81f6e37865942ac800f980aa2a5e",
+    "url": "/static/media/fahad_hayat.a9be81f6.jpg"
+  },
+  {
+    "revision": "cef50fc165dd17d6062e0b8c2b05fbf6",
+    "url": "/static/media/fahadhayat.cef50fc1.pdf"
+  },
+  {
+    "revision": "a5e87149af70db8b7102e53a6fdb02b6",
+    "url": "/static/media/up-arrow-circle.a5e87149.svg"
+  }
+]);
